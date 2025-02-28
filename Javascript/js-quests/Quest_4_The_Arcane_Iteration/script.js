235c7340e38f6d5489e2129e0a571454501a0d6d
chetanuchiha16 <chetan16ck@gmail.com>
2025-02-28 18:50:51 +0530

    Add new quest iteration with HTML, CSS, and JavaScript files

---
+++ Javascript/js-quests/Quest_4_The_Arcane_Iteration/script.js
@@ -0,0 +1,33 @@
+hello(bye);
+
+sum(display, 1,2);
+
+
+function sum(callback, a, b) {
+  result = a + b;
+  callback(result);
+}
+
+function display(result) {
+  document.body.innerHTML = `<h1>the sum is ${result}<h1/>`;
+}
+
+function hello(callback) {
+  let str = `hey hinaaa..`;
+  callback(str);
+}
+
+function bye(str) {
+  // console.log(`hinaaa.. ill never say bye to you`)
+  console.log(str)
+}
+
+
+let arr = [1,2,3,4,5];
+
+arr.forEach(displayEle);
+
+function displayEle(ele) {
+  console.log(ele);
+  
+}
