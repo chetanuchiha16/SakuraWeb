0c76ccce7ea48293991cb9aea91d7699fcd548b4
chetanuchiha16 <chetan16ck@gmail.com>
2025-03-02 09:44:10 +0530

    forEach

--- Javascript/js-quests/Quest_4_The_Arcane_Iteration/script.js
+++ Javascript/js-quests/Quest_4_The_Arcane_Iteration/script.js
@@ -29,8 +29,29 @@ function displayEle(ele) {
   console.log(ele);
 }
 
-function double(element) {
-  console.log(element * 2);
+// function double(element) {
+//   console.log(element * 2);
+// }
+
+function double(number, index, array) {
+  array[index] = number * 2;
 }
 
+arr.forEach((element, index, array) => {
+  array[index] = element * 3;
+});
 arr.forEach(double);
+arr.forEach(displayEle);
+
+arr.forEach((element, index, array) => {
+  array[index] = element / 3;
+});
+arr.forEach(displayEle);
+arr.forEach((element, index, array) => {
+  array[index] = element / 2;
+});
+arr.forEach(displayEle);
+
+let array = [6, 5, 4, 3, 2, 1];
+array = array.map(element => element * 2)
+array.forEach(displayEle);
